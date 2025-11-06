58165d63f6016befeddafb440f0cc3fbdd92266c
Lourds-Sabila-Pushpa <L0023_Lourds@Leniinfotech.in>
2025-11-06 21:54:49 +0530

    test

--- ERP/Controllers/MASTER/VendorController.cs
+++ ERP/Controllers/MASTER/VendorController.cs
@@ -120,7 +120,7 @@ namespace ERP.Controllers.MASTER
             }
         }
 
-        //this tis to check the vendor exist or not
+        //this tis to check the vendor exist or not in this
         private bool VendorExists(int id)
         {
             return _context.Vendor.Any(e => e.ID == id);
