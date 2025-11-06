2889427c895a141c4bec03550873d62819fa68ed
Lourds-Sabila-Pushpa <L0023_Lourds@Leniinfotech.in>
2025-11-06 21:42:07 +0530

    comment added in controller

--- ERP/Controllers/MASTER/VendorController.cs
+++ ERP/Controllers/MASTER/VendorController.cs
@@ -120,6 +120,7 @@ namespace ERP.Controllers.MASTER
             }
         }
 
+        //this tis to check the vendor exist or not
         private bool VendorExists(int id)
         {
             return _context.Vendor.Any(e => e.ID == id);
