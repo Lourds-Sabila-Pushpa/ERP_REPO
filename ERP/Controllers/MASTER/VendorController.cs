f7bbf7918097eb2353f595d5dae3d185c9a96910
Lourds-Sabila-Pushpa <L0023_Lourds@Leniinfotech.in>
2025-11-07 11:51:01 +0530

    TEST

--- ERP/Controllers/MASTER/VendorController.cs
+++ ERP/Controllers/MASTER/VendorController.cs
@@ -120,7 +120,7 @@ namespace ERP.Controllers.MASTER
             }
         }
 
-        //this is url
+        //this is url o
         private bool VendorExists(int id)
         {
             return _context.Vendor.Any(e => e.ID == id);
